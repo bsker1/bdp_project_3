6f0c817b2a7040bb0e6043055697afb43ac0039f
NRL1213 <122542061+NRL1213@users.noreply.github.com>
2023-11-20 15:05:31 -0500

    Work on wordcount factor
    Write the code to get the word count as a factor and apply it to the new dataframe

--- Factor Finder/FactorBuilder.py
+++ Factor Finder/FactorBuilder.py
@@ -4,7 +4,24 @@ import pandas as pd
 import statsmodels as sm
 
 def main():
-    FactorFrame = pd.DataFrame()
-    EssayFrame = pd.read_csv("DataSubSet")
+    EssayFrame = pd.read_csv("../raw_data.csv")
 
+    FactorFrame = EssayFrame.copy(deep=False)
+
+    #Data cleaning
+    #Thanks to chatgpt for this wonderful 1 liner to drop all essays with less than 50 words
+    FactorFrame.drop(FactorFrame[FactorFrame["text"].apply(lambda x: len(x.split()) < 50)].index)
+
+    # Get Word Count
+    FactorFrame.insert(column="WordCount", loc=2, value=0)
+    for pos, row in FactorFrame.iterrows():
+            FactorFrame.at[pos, "WordCount"]= len(row["text"].split())
+
+    #Print Dataframe
+    print(FactorFrame)
+
+    #Make sure to write new dataframe
+    #Write dataframe to csv here...
     
+if __name__ == '__main__':
+    main()
