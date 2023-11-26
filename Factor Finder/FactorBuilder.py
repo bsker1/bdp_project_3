f7572ea9ee7843aef07b21632f5a8ff98192e13d
NRL1213 <122542061+NRL1213@users.noreply.github.com>
2023-11-26 16:28:32 -0500

    Build out the weight Adjuster prediction
    Remove the old named data sets and create 3 models. One is a statsmodel logestic regression which tells us how well their model fits. The next two are both scikit logistic regressions and their accuracy score at the end is based on A training set and a test set. We obtain their score in order to prove which model is the most accurate. We expect the balanced weight one to be the most accurate as itll auto balance itself with coefficients but if the second model is close we have converged to the best weights

--- Factor Finder/FactorBuilder.py
+++ Factor Finder/FactorBuilder.py
@@ -4,52 +4,58 @@ import re
 from readability import Readability
 
 def main():
-    EssayFrame = pd.read_csv("../sampled_data.csv")
+    FileNames = ["../sampled_data.csv", "../training_data.csv"]
 
-    FactorFrame = EssayFrame.copy(deep=False)
+    for Name in FileNames:
 
-    #Data cleaning
-    #Thanks to chatgpt for this wonderful 1 liner to drop all essays with less than 100 words
-    FactorFrame.drop(FactorFrame[FactorFrame["text"].apply(lambda x: len(x.split()) < 101)].index)
+        EssayFrame = pd.read_csv(Name)
 
-    # Get Word Count
-    FactorFrame.insert(column="WordCount", loc=2, value=0)
-    for pos, row in FactorFrame.iterrows():
-        FactorFrame.at[pos, "WordCount"] = len(row["text"].split())
+        FactorFrame = EssayFrame.copy(deep=False)
 
-    # Get pronoun Count
-    Pronouns = re.compile(r'\b(I|me|myself|mine|my|we|us|ourselves|ours|our|Me|Myself|Mine|My|We|Us|Outselves|Ours|Our)\b')
-    FactorFrame.insert(column="PronounCount", loc=3, value=0)
-    for pos, row in FactorFrame.iterrows():
-        FactorFrame.at[pos, "PronounCount"] = len(Pronouns.findall(row["text"]))
+        #Data cleaning
+        #Thanks to chatgpt for this wonderful 1 liner to drop all essays with less than 100 words
+        FactorFrame.drop(FactorFrame[FactorFrame["text"].apply(lambda x: len(x.split()) < 101)].index)
 
-    # Get Dale-Chall Readabilty
-    # import nltk
-    # nltk.download()
-    # Run the commands above in the python terminal and download all
-    FactorFrame.insert(column="Readabilty", loc=4, value=0)
-    for pos, row in FactorFrame.iterrows():
-        FactorFrame.at[pos, "Readabilty"] = Readability(row["text"]).dale_chall().score
+        # Get Word Count
+        FactorFrame.insert(column="WordCount", loc=2, value=0)
+        for pos, row in FactorFrame.iterrows():
+            FactorFrame.at[pos, "WordCount"] = len(row["text"].split())
 
-    # Get Paragraph Count
-    FactorFrame.insert(column="ParagraphCount", loc=5, value=0)
-    for pos, row in FactorFrame.iterrows():
+        # Get pronoun Count
+        Pronouns = re.compile(r'\b(I|me|myself|mine|my|we|us|ourselves|ours|our|Me|Myself|Mine|My|We|Us|Outselves|Ours|Our)\b')
+        FactorFrame.insert(column="PronounCount", loc=3, value=0)
+        for pos, row in FactorFrame.iterrows():
+            FactorFrame.at[pos, "PronounCount"] = len(Pronouns.findall(row["text"]))
+
+        # Get Dale-Chall Readabilty
+        # import nltk
+        # nltk.download()
+        # Run the commands above in the python terminal and download all
+        FactorFrame.insert(column="Readabilty", loc=4, value=0)
+        for pos, row in FactorFrame.iterrows():
+            FactorFrame.at[pos, "Readabilty"] = Readability(row["text"]).dale_chall().score
+
+        # Get Paragraph Count
+        FactorFrame.insert(column="ParagraphCount", loc=5, value=0)
+        for pos, row in FactorFrame.iterrows():
             FactorFrame.at[pos, "ParagraphCount"] = len(row["text"].split("\n"))
 
-    # Get Paragraph size choesion (Standard Deviation)
-    FactorFrame.insert(column="ParagraphSizeCohesion", loc=6, value=0)
-    for pos, row in FactorFrame.iterrows():
-        ParagraphList = row["text"].split("\n")
-        ParagraphSizes = []
-        for Paragraph in ParagraphList:
-            ParagraphSizes.append(len(Paragraph))
-        FactorFrame.at[pos, "ParagraphSizeCohesion"] = np.std(ParagraphSizes)
+        # Get Paragraph size choesion (Standard Deviation)
+        FactorFrame.insert(column="ParagraphSizeCohesion", loc=6, value=0)
+        for pos, row in FactorFrame.iterrows():
+            ParagraphList = row["text"].split("\n")
+            ParagraphSizes = []
+            for Paragraph in ParagraphList:
+                ParagraphSizes.append(len(Paragraph))
+            FactorFrame.at[pos, "ParagraphSizeCohesion"] = np.std(ParagraphSizes)
 
-    #Print Dataframe
-    print(FactorFrame)
+        #Print Dataframe
+        print(FactorFrame)
 
-    #Make sure to write new dataframe
-    FactorFrame.to_csv("../FactorizedData.csv")
+        #Make sure to write new dataframe
+        prefix = Name[:-4]
+        file = prefix + "_Factorized.csv"
+        FactorFrame.to_csv(file)
     
 if __name__ == '__main__':
     main()
--- Model Trainer/ModelTrainer.py
+++ Model Trainer/ModelTrainer.py
@@ -4,30 +4,75 @@ import pandas as pd
 import statsmodels.api as sm
 import sklearn as sk
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
 
 def main(rng):
-    TrainingFrame = pd.read_csv("../FactorizedData.csv")
+    TrainingFrame = pd.read_csv("../training_data_Factorized.csv")
+    TestFrame = pd.read_csv("../sampled_data_Factorized.csv")
 
-    #endog
+    #Training Sets
     ResponseCollection = TrainingFrame["label"]
-
-    #exog
     Input = TrainingFrame[["WordCount", "PronounCount", "Readabilty", "ParagraphCount", "ParagraphSizeCohesion"]].to_numpy()
 
-    #Statsmodel Logistic regression (needed to classifiy binary outputs)
+    #Test Sets
+    TestResponse = TestFrame["label"]
+    TestInput = TestFrame[["WordCount", "PronounCount", "Readabilty", "ParagraphCount", "ParagraphSizeCohesion"]].to_numpy()
+
+    Xtrain = Input
+    Xtest = TestInput
+    Ytrain = ResponseCollection
+    Ytest = TestResponse
+
+    print("Stats model regression stats")
+    print("////////////////////////////")
     model = sm.Logit(ResponseCollection, Input)
     results = model.fit()
     print(results.summary())
 
-    Xtrain = Input
-    Xtest = None #Get a dataframe of factorized test data
-    Ytrain = ResponseCollection
-    Ytest = None #Get a series of labels of Xtest data
+    print("/////////////////////////////")
 
+
+    #Scikit logestic regression (Balanced)
+    model = LogisticRegression()
+    model.fit(Xtrain, Ytrain)
+
+    #Scikit auto adjusting weight regression
     Contestants = 10
-    StartWeight = None #Create a dict of labels and weights constant
-    #Apply randomness
-    #Run test selected the best R^2 and redo randomness
+    StandardStartWeight = {"WordCount": 0.5, "PronounCount": 0.5, "Readabilty":0.5, "ParagraphCount":0.5, "ParagraphSizeCohesion": 0.5, 0:1, 1:12}
+    StartWeightList = [StandardStartWeight]
+
+    for Trails in range(5):
+        for ContestantNo in range(Contestants - 1):
+            RngList = {}
+            for key in StartWeightList[0].keys():
+                RngList[key] = StandardStartWeight[key] + StandardStartWeight[key] * np.random.uniform(low = -0.2, high = 0.2)
+            StartWeightList.append(RngList)
+
+        AccuracyList = []
+
+        for Contestant in StartWeightList:
+            regression = LogisticRegression(class_weight=Contestant, max_iter=100)
+            regression.fit(Xtrain, Ytrain)
+            predictions = regression.predict(Xtest)
+            accuracy = accuracy_score(Ytest, predictions)
+            AccuracyList.append(accuracy)
+
+
+        BestIndex = AccuracyList.index(max(AccuracyList))
+        StandardStartWeight = StartWeightList[BestIndex]
+        print("Accuracy is now at :")
+        print(AccuracyList[BestIndex])
+        print("Weight is")
+        print(StartWeightList[BestIndex])
+
+    WeightedAdjusterScore = regression.score(Xtest, Ytest)
+    BalancedWeight = model.score(Xtest, Ytest)
+
+    print("Weight adjuster scored at:")
+    print(WeightedAdjusterScore)
+    print("Balanced weight scored at:")
+    print(BalancedWeight)
 
 if __name__ == '__main__':
     rng = np.random.default_rng()
